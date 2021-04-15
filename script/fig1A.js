21f99c8f34c77166aee2cc39526e5051f46b71cd
kawilliams <kawilliams@email.arizona.edu>
2021-04-15 10:19:12 -0400

    Add points and cutoff lines:

--- script/fig1A.js
+++ script/fig1A.js
@@ -30,7 +30,7 @@ d3.csv("script/dummy-data.csv").then(function(d){
 
 	// // y: Number of active chronic conditions
 	var y = d3.scaleLinear()
-		.domain(d3.extent(d, function(d){ return d.score; }))
+		.domain([0, Math.ceil(d3.max(d, function(d){ return d.score; }))])
 		.range([height - margin.bottom, margin.top]);
 
 	var yAxis = g => g
@@ -60,7 +60,51 @@ d3.csv("script/dummy-data.csv").then(function(d){
 	svg.append('g')
 		.call(yAxis);
 
+	// Add percentile lines marking "defaulted" (95%) and "referred" (55%)
+	var cutoffLines = [55, 95];
+	svg.append('g')
+		.selectAll('line')
+		.data(cutoffLines)
+		.join('line')
+			.attr('x1', d => x(d))
+			.attr('x2', d => x(d))
+			.attr('y1', d => y(0))
+			.attr('y2', margin.top)
+			.attr('stroke', d => (d == 55) ? 'lightgrey' : 'black')
+			.attr('stroke-width', 2)
+			.attr('stroke-dasharray', 8);
 
+	svg.append('g')
+		//.attr('transform', 'translate(' + margin.left + ','+ margin.top+')')
+		.selectAll('circle')
+			.data(d)
+			.join('circle')
+			.attr('cx', d => x(d.percentile))
+			.attr('cy', d => y(d.score))
+			.attr('r', 3)
+			.attr('fill', d => (d.race == 'B') ? 'purple' : 'orange')
+			.attr('stroke', d => (d.race == 'B') ? 'purple' : 'orange');
+
+	var label = svg.append('g')
+		.attr('font-family', 'sans-serif')
+		.attr('font-size', 10)
+		.selectAll('g')
+		.data(d)
+		.join('g')
+		.attr('transform', d => 'translate(' + x(d.percentile) + ',' + y(d.score) + ')')
+		.attr('opacity', 1);
+	label.append('text')
+		.text(d => d.score + " " + d.race)
+		.each(function(d){
+			const p = d3.select(this);
+			switch(d.orient) {
+				case "top": p.attr('text-anchor', 'middle').attr('dy', '-0.7em');break;
+				case "right": p.attr('dx', '0.5em').attr('dy', '0.32em').attr('text-anchor','start');break;
+				case "bottom": p.attr('text-anchor', 'middle').attr('dy','1.4em');break;
+				case "left": p.attr('dx', '-0.5em').attr('dy', '0.32em').attr('text-anchor','end');break;
+				
+			}
+		});
 
 })
 .catch(function(error){
