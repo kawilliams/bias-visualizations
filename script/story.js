ee0581db8dea17d2738d6a00ac82eb1be49f50e9
kawilliams <kawilliams@email.arizona.edu>
2021-04-23 15:02:02 -0400

    Fixing popup texts

--- script/story.js
+++ script/story.js
@@ -1,5 +1,5 @@
 
-var margin = ({top: 50, right: 10, bottom: 30, left: 50});
+var margin = ({top: 30, right: 30, bottom: 30, left: 30});
 
 var circleBox = 12;
 var algBoxSize = {height: 30, width: 60};
@@ -42,7 +42,7 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 	patients.append('circle')
 		.attr('class', 'dots exampleGlow')
 		.attr('cx', d => d.x * circleBox + margin.left)
-		.attr('cy', d => d.y * circleBox + margin.top )
+		.attr('cy', d => d.y * circleBox + margin.top + 10)
 		.attr('r', radius)
 		.attr('fill', '#EDC951');
 
@@ -50,7 +50,7 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 		.attr('class', 'label')
 		.attr('transform', d => {
 			var x = d.x * circleBox + margin.left - 2;
-			var y = d.y * circleBox + margin.top + 2;
+			var y = d.y * circleBox + margin.top + 12;
 			return "translate("+ x +","+ y +")";
 		})
 		.text(d => d.race)
@@ -68,7 +68,7 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 	var algorithmBox = algorithmG
 		.append('rect')
 		.attr('class', 'alg')
-		.attr('x', viewBoxSize.width * 0.35)
+		.attr('x', viewBoxSize.width * 0.37)
 		.attr('y', viewBoxSize.height * 0.28)
 		.attr('height', algBoxSize.height)
 		.attr('width', algBoxSize.width)
@@ -86,56 +86,50 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 
 	var inputBoxG = svg.append('g');
 	var inputBoxRect = inputBoxG.append('rect')
-		.attr('x', 10)
-		.attr('y', 10)
+		.attr('x', viewBoxSize.width * 0.1)
+		.attr('y', viewBoxSize.height * 0.1)
 		.attr('rx', 3)
-		.attr('height', 100)
+		.attr('height', 90)
 		.attr('width', 50)
 		.attr('fill', 'lightsteelblue');
 
-	var inputTextList = [{'input': "initial", 
-			"value":" Insurance claims info\n\
-			Age\n\
-			Number of doctors' visits\n\
-			..." },
-			{'input':'revised',
-			"value":"Insurance claims info:\n\
-			Age\n\
-			Number of doctors' visits\n\
-			...\n\
-			Health info:\n\
-			Diagnoses\n\
-			Metrics\n\
-			..."}];
+	var inputText = "Insurance claims info\n\
+				Age\n\
+				Number of doctors' visits\n\
+				...\n\
+				Health info:\n\
+				Diagnoses\n\
+				Metrics\n\
+				...";
 
 	var inputBoxText = inputBoxG.selectAll('text')
-		.data(inputTextList)
+		.data(d => inputText.split("\n"))
 		.enter()
 		.append('text')
-		.attr('x', 14)
-		.attr('y', 30);
-		inputBoxText.selectAll('tspan')
-		.data(inputTextList => inputTextList.value.split("\n"))
-		.enter()
-		.append('tspan')
+		.attr('x', viewBoxSize.width * 0.1)
+		.attr('y', viewBoxSize.height * 0.1);
+
+	inputBoxText.append('tspan')
 		.attr('class', 'text')
-		.text(d => d)
-		.attr('x', 14)
+		.text((d,i) => d)
+		.attr('x', viewBoxSize.width * 0.1)
+		.attr('y', (d,i) => i * 6 + margin.top)
+		.attr('display', (d,i) => (i < 4) ? 'inline' : 'none')
 		.attr('dy',6)
-		.attr('dx',0);
+		.attr('dx',3);
 
 	var nextBoxG = svg.append('g');
 	var nextBox = nextBoxG.append('rect')
 		.attr('class', 'next')
-		.attr('x', 110)
+		.attr('x', viewBoxSize.width * 0.6 + margin.left)
 		.attr('y', 85)
 		.attr('height', 5)
-		.attr('display', 'visible')
+		.attr('display', 'inline')
 		.on('click', showPopUp);
 	var nextBoxText = nextBoxG.append('text')
 		.attr('class', 'next')
 		.text('NEXT')
-		.attr('x', 112)
+		.attr('x', viewBoxSize.width * 0.6 + margin.left + 3)
 		.attr('y', 90)
 		.attr('opacity', 0)
 		.attr('cursor', 'pointer')
@@ -152,26 +146,40 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 		.attr('display', 'none')
 		.on('click', closePopUp);
 
-	popUpBoxG.append('text')
-		.attr('id', 'popuptext')
-		.attr('class', 'popup')
+	var text0 = "Remember, we used an algorithm\n\
+				to determine a 'sick' score for \n\
+				each patient. Let's see how the \n\
+				algorithm did, and see if we \n\
+				managed to get all of the sick\n\
+				patients into the box."
+	var popUpBoxText = popUpBoxG.selectAll('text')
+		.data(d => text0.split('\n'))
+		.enter()
+		.append('text')
+		.attr('class', 'popuptext')
 		.attr('x', 10)
-		.attr('y', 10)
-		.text("Let's check our algorithm")
+		.attr('y', 10);
+		popUpBoxText.append('tspan')
+		.attr('class', 'popuptext')
+		.text(d => d)
+		.attr('x', 10)
+		.attr('y', (d,i) => i * 6 + margin.top)
 		.attr('display', 'none');
+
+
 
 
 	function initialSickFilter(d){
 		d3.selectAll('.dots')
 			.transition()
 			.attr('fill', d => (d.x >= 2) ? '#84b046' : 'lightgrey'); //green
-		// d3.selectAll(".exampleGlow")
-		// 	.style('filter', 'url(#glow)');
+		d3.selectAll(".exampleGlow")
+			.style('filter', 'url(#glow)');
 
 		d3.select('rect.next')
 			.transition()
 			.attr('height', 7)
-			.attr('width', 15)
+			.attr('width', 17)
 			.attr('rx', 3)
 			.attr('display', 'inline')
 			.attr('fill', 'lightsteelblue');
@@ -183,17 +191,17 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 		d3.select('#popupbox')
 			.transition()
 			.attr('display','inline')
-			.attr('x', 50)
+			.attr('x', 100)
 			.attr('y', 30)
 			.attr('fill', 'white')
 			.attr('stroke', 'lightsteelblue')
-			.attr('height', 100)
-			.attr('width', 200)
+			.attr('height', 65)
+			.attr('width', 100)
 			.attr('opacity', 0.9);
-		d3.select('#popuptext')
+		d3.selectAll('.popuptext')
 			.transition()
-			.attr('x', 60)
-			.attr('y', 50)
+			.attr('x', 120)
+			.attr('y', (d,i) => i * 4 + 40)
 			.attr("display", 'inline');
 
 		d3.selectAll('.next')
@@ -202,55 +210,49 @@ d3.csv('data/patient-dot-data.csv').then(function(d){
 	}
 
 	function closePopUp() {
-		d3.selectAll('.popup')
-			.transition()
-			.attr('display', 'none');
-		d3.select('#popuptext').remove();
+		// d3.selectAll('.popup')
+		// 	.transition()
+		// 	.attr('display', 'none');
+		//d3.select('#popuptext').remove();
+
 		recolorPatients();
 
 		d3.select('#popupbox')
-			.transition()
 			.attr('display','inline')
 			.attr('x', viewBoxSize.width * 0.72)
-			.attr('y', 10)
+			.attr('y', 0)
 			.attr('fill', 'white')
 			.attr('stroke', 'lightsteelblue')
-			.attr('height', 30)
-			.attr('width', 60)
+			.attr('height', 50)
+			.attr('width', 80)
 			.attr('opacity', 0.9);
+		
 		var text1 = "Uh oh - some sick folks didn't make it into\n\
 					the program. And if we look closer, we see most\n\
 					of the patients who didn't make it are Black. \n\
 					On the flip side, most of the healthy people who\n\
 					got into the program are White.\n\nThis isn't fair."
-		
-		var popUpText = d3.select('#popupbox')
-		.append('text')
-		.attr('id', 'popuptext')
-		.attr('class', 'popup')
+
+		d3.select('#popuptext')
+		.attr('display', 'inline')
 		.attr('x', viewBoxSize.width * 0.72)
 		.attr('y', 20)
-		.attr('fill', 'black');
-		// var instructions = popup.select('text')
-		// 	.data(text1)
-		// 	.enter()
-		// 	.append('text')
-		// 	.attr('x', viewBoxSize.width * 0.72)
-		// 	.attr('y', 20);
-		popUpText.selectAll('tspan')
-			.data(d => text1.split('\n'))
-			.enter()
-			.append('tspan')
+		.text('');
+
+		var popUpText = d3.select('#popuptext')
+		.data(d => text1.split('\n'));
+		// .enter()
+		// .append('text')
+		// .attr('class', 'popup');
+		popUpText.append('tspan')
 			.attr('class', 'popup')
 			.text(d => d)
 			.attr('x', viewBoxSize.width * 0.72)
-			.attr('y', 30)
-			.attr('opacity', 1)
-			.attr('fill', 'black');
+			.attr('y', (d,i) => i * 4 + 10);
 
-		d3.selectAll('.next')
-			.transition()
-			.attr('display', 'none');
+		// d3.selectAll('.next')
+		// 	.transition()
+		// 	.attr('display', 'none');
 
 	}
 
