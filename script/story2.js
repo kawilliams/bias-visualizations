5205400abdd122079816e0cbd8589b9c2e95095c
kawilliams <kawilliams@email.arizona.edu>
2021-04-29 13:38:31 -0400

    MVC works, can click through steps:

--- script/story2.js
+++ script/story2.js
@@ -25,9 +25,7 @@ var makeObservers = function() {
 			_subscribers.push(s);
 		},
 		notify: function(args) {
-			console.log("Notify "+_subscribers.length+" listeners about ", args);
 			for (var i = 0; i < _subscribers.length; i++) {
-				console.log(_subscribers[i]);
 				_subscribers[i](args);
 	    	}
 		}
@@ -37,7 +35,6 @@ var makeObservers = function() {
 var makeModel = function(data) {
 	//Make observers that can be notified
 	var _observers = makeObservers();
-	console.log("Model observers exist", _observers);
 
 	// The storyboard step (0 - 8)
 	var _step = 0;
@@ -45,7 +42,8 @@ var makeModel = function(data) {
 	var _data = data;
 
 	var _text = [
-		"text 0 text 0 text 0 text 0 text 0 text 0 text 0 text 0 text 0 text 0 text 0 \n more text more text more text more text more text more text more text more text more text",
+		"Below are 10 patients with varying levels of health and only 5 of them can be accepted into the health program.\n\
+		We want to line them up from sickest to healthiest. We'll use an algorithm to determine the level of health.",
 		"text 1 \n more text",
 		"text 2 \n more text",
 		"text 3 \n more text",
@@ -58,10 +56,9 @@ var makeModel = function(data) {
 	return {
 		//Increment the step & tell everyone
 		increment: function() {
-			console.log('model increment');
 			_step += 1;
+			_step = _step % 7; 
 			_observers.notify();
-			console.log("**Increment complete\n**Observers notified to be at step", _step);
 		},
 		//Get the step
 		get: function() {
@@ -88,7 +85,6 @@ var makeModel = function(data) {
 var makeSVGView = function(model, data, svgID) {
 	//Make observers that can be notified
 	var _observers = makeObservers();
-	console.log("SVG observers exist", _observers);
 
 	var _svg = d3.select(svgID)
 		.attr('preserveAspectRatio', 'xMidYMid meet')
@@ -117,17 +113,24 @@ var makeSVGView = function(model, data, svgID) {
 	}
 
 	var _makeTopText = function(step, text) {
-		
-		var topText = _svg.selectAll('text')
-			.data(d => text[step].split('\n'))
-			.enter().append('text')
-			.attr('x', d =>  viewBoxSize.width * 0.5 - d.length )
+		_svg.selectAll('.toptext').remove();
+		var topText = _svg
+			// .selectAll('text')
+			// .data(d => text)
+			// .enter()
+			.append('text')
+			// .attr('x', d =>  viewBoxSize.width * 0.5 - d.length )
+			.attr('x', viewBoxSize.width * 0.5)
 			.attr('y', margin.top)
+			.attr('class', 'toptext')
 			.attr('font-size', '12px')
 			.attr('text-align', 'center');
 
-		topText.append('tspan')
-			.text(d => d.trim())
+		topText.selectAll('tspan')
+			.data(d => text[step].split('\n'))
+			.enter()
+			.append('tspan')
+			.text(d => d)
 			.attr('x', d =>  viewBoxSize.width * 0.5 - d.length )
 			.attr('y', (d,i) => i * 7 + margin.top);
 
@@ -151,24 +154,24 @@ var makeSVGView = function(model, data, svgID) {
 
 var makeButtonView = function(model, data, buttonID, svgID) {
 	var _observers = makeObservers();
-	console.log("Button observers exist", _observers);
 
 	var _btn = d3.select(svgID)
-		.append('rect')
-		.attr('id', buttonID)
-		.attr('x', viewBoxSize.width * 0.5 - 10)
-		.attr('y', viewBoxSize.height - margin.bottom)
-		.attr('width', 24)
-		.attr('height', 12)
-		.attr('cursor', 'pointer')
-		.style('fill', 'lightsteelblue')
-		.style('rx', 3);
+	.append('rect')
+	.attr('id', buttonID)
+	.attr('x', viewBoxSize.width * 0.5 - 10)
+	.attr('y', viewBoxSize.height - margin.bottom)
+	.attr('width', 30)
+	.attr('height', 12)
+	.attr('cursor', 'pointer')
+	.style('fill', 'lightsteelblue')
+	.style('rx', 3);
 
 	var _btnText = d3.select(svgID)
 		.append('text')
 		.attr('x', viewBoxSize.width * 0.5 - 5)
 		.attr('y', viewBoxSize.height - margin.bottom + 7)
-		.text('NEXT: K')
+		.text('NEXT: 0')
+		.attr('cursor', 'pointer')
 		.style('font-size', '6px');
 
 	// The button event passes the appropriate
@@ -179,17 +182,13 @@ var makeButtonView = function(model, data, buttonID, svgID) {
 		});
 	};
 
-	var _currentStep = model.get();
-
 	_btn.on('click', _fireIncrementEvent);
+	_btnText.on('click', _fireIncrementEvent);
+
 
 	return {
 		render: function() {
-
-			var _currentStep = model.get();
-			var t = _btnText.text().replace("K", _currentStep);
-			_btnText.text(t);
-			
+			_btnText.text("NEXT: " + model.get());
 		},
 		register: function(fxn) {
 			_observers.add(fxn);
@@ -200,12 +199,10 @@ var makeButtonView = function(model, data, buttonID, svgID) {
 
 var makeController = function(model) {
 	var _increment = function() {
-		console.log("controller telling model to increment");
 		model.increment();
 	}
 	return {
 		dispatch: function(evt) {
-			console.log("Controller dispatch:", evt);
 			if (evt){
 				switch(evt.type){
 				case story.signal.increment:
@@ -236,8 +233,8 @@ document.addEventListener("DOMContentLoaded", function(event){
 		story.controller = makeController(story.model);
 		
 		for (var i=0; i < story.views.length; i++) {
-			story.model.register(story.views[i].render());
-			story.views[i].register(story.controller.dispatch());
+			story.model.register(story.views[i].render);
+			story.views[i].register(story.controller.dispatch);
 		}
 
 	}) 
